a2e0f23474e4d4161615b7efa6195a1b10d413f1
Пшенін Богдан Олександрович <pshenin@fuib.com>
2021-11-19 17:42:14 +0200

    homework06

---
+++ hw06/src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../card/card";
+import data from "../../data/data";
+
+function App(){
+    return(
+        <>
+        <Card data={data}/>
+        </>
+    )
+}
+export default App
